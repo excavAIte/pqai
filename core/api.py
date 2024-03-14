cc5156088ef0ff1fb38c4223b7f55eef9849aa5b
Prateek Sane <ptk1917@gmail.com>
2024-03-13 19:33:35 -0700

    update pqai for pubnum

--- core/api.py
+++ core/api.py
@@ -262,6 +262,7 @@ class SearchRequest102(SearchRequest):
         results = self._get_results()
         if self._n_results < 100:
             results = self._rerank(results)
+        print(results[0].abstract)
         results = self._deduplicate(results)
         results = results[:self._n_results]
         return results[self._offset:]
