4902f28544fd30f54eab12b16652cf8d70b7b633
mlsimon734 <mlsimon734@gmail.com>
2024-05-29 22:26:55 -0700

    linting + python version fix

--- scripts/mongo2faiss.py
+++ scripts/mongo2faiss.py
@@ -4,17 +4,18 @@ Create FAISS indexes for documents pulled from a Mongo DB collection
 This script can be modified for creating indexes of documents from any source
 """
 
-import os
+# import os
 import sys
-import json
+
+# import json
 from pathlib import Path
 import numpy as np
 from tqdm import tqdm
 from pymongo import MongoClient
 
-MONGO_URI = 'mongodb://localhost:27017'
-MONGO_DB = 'excavaite'
-MONGO_COLL = 'patent_data'
+MONGO_URI = "mongodb://localhost:27017"
+MONGO_DB = "excavaite"
+MONGO_COLL = "patent_data"
 THIS_DIR = str(Path(__file__).parent.resolve())
 BASE_DIR = str(Path(__file__).parent.parent.resolve())
 INDEXES_DIR = "{}/indexes".format(BASE_DIR)
@@ -22,15 +23,16 @@ INDEXES_DIR = "{}/indexes".format(BASE_DIR)
 INDEX_NAME = "patent_history"
 
 sys.path.append(BASE_DIR)
-from core.indexes import FaissIndex, FaissIndexReader
-from core.vectorizers import SentBERTVectorizer
+from core.indexes import FaissIndex  # FaissIndexReader # noqa
+from core.vectorizers import SentBERTVectorizer  # noqa
 
 vectorizer = SentBERTVectorizer()
-N_DIMS = vectorizer.embed('sample string').shape[0]
+N_DIMS = vectorizer.embed("sample string").shape[0]
+print("Embedding dimension: {}".format(N_DIMS))
 
 client = MongoClient(MONGO_URI)
 collection = client[MONGO_DB][MONGO_COLL]
-query = {'publicationDate': {'$gte': '20010807'}}
+query = {"publicationDate": {"$gte": "20010807"}}
 
 N = collection.count_documents(query)
 cursor = collection.find(query)
@@ -39,21 +41,21 @@ labels = []
 vectors = np.zeros((N, N_DIMS))
 for i in tqdm(range(N)):
     if not cursor.alive:
-        raise Exception('Mongo cursor terminated prematurely!')
+        raise Exception("Mongo cursor terminated prematurely!")
     doc = cursor.next()
-    dn = doc['publicationNumber']
+    dn = doc["publicationNumber"]
     text = []
-    text.append(doc['title'])
-    text.append(doc['abstract'])
-    text.append(doc['description'])
-    text.append(doc['claimInfo'])
-    text = ' '.join([t for t in text if t is not None])
+    text.append(doc["title"])
+    text.append(doc["abstract"])
+    text.append(doc["description"])
+    text.append(doc["claimInfo"])
+    text = " ".join([t for t in text if t is not None])
     vector = vectorizer.embed(text)
     labels.append(dn)
     vectors[i] = vector
 
-print('Indexing...')
+print("Indexing...")
 index = FaissIndex(name=INDEX_NAME)
 index._index_dir = INDEXES_DIR
 index.add_vectors(vectors, labels)
-print('Index saved in {}'.format(INDEXES_DIR))
+print("Index saved in {}".format(INDEXES_DIR))
