224ed7bed3b5f7ca73424abd34da09eab88d2fa6
mlsimon734 <mlsimon734@gmail.com>
2024-06-20 20:17:27 -0700

    linting

--- core/index_selection.py
+++ core/index_selection.py
@@ -1,16 +1,17 @@
 from core.classifiers import BOWSubclassPredictor
 
-class SubclassBasedIndexSelector():
 
-	_subclass_predict_fn = BOWSubclassPredictor().predict_subclasses
-	
-	def __init__(self, indexes):
-		self._indexes = indexes
+class SubclassBasedIndexSelector:
 
-	def select(self, text, n=3):
-		subclasses = self._subclass_predict_fn(text)
-		subclasses = subclasses[:n]
-		indexes = []
-		for subclass in subclasses:
-			indexes += self._indexes.get(subclass)
-		return indexes
+    _subclass_predict_fn = BOWSubclassPredictor().predict_subclasses
+
+    def __init__(self, indexes):
+        self._indexes = indexes
+
+    def select(self, text, n=3):
+        subclasses = self._subclass_predict_fn(text)
+        subclasses = subclasses[:n]
+        indexes = []
+        for subclass in subclasses:
+            indexes += self._indexes.get(subclass)
+        return indexes
